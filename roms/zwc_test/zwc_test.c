c8d847d438c80e402d1d1dfe64729328c2646de7
Koen Martens <gmc@sonologic.nl>
2014-03-05 22:57:39 +0100

    rom: add zwc test

---
+++ roms/zwc_test/zwc_test.c
@@ -0,0 +1,80 @@
+/*
+ * Shows usage of interrupts. Goes along with zpu_core_small_wip.vhd.
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <gmzpu_io.h>
+
+volatile int counter;
+
+/* Example of single, fixed interval non-maskable, nested interrupt. The interrupt signal is
+ * held high for enough cycles to guarantee that it will be noticed, i.e. longer than
+ * any io access + 4 cycles roughly.
+ * 
+ * Any non-trivial interrupt controller would have support for
+ * acknowledging interrupts(i.e. keep interrupts asserted until
+ * software acknowledges them via memory mapped IO).
+ */
+void  _zpu_interrupt(void)
+{
+	/* interrupts are enabled so we need to finish up quickly,
+	 * lest we will get infinite recursion!*/
+	counter++;
+}
+
+static const char *hex_digits="0123456789abcdef";
+
+void print_hex(__uint32_t dat)
+{
+    int i;
+    char out[9];
+
+    for(i=7;i>=0;i--) {
+        out[i]=hex_digits[dat & 0xf];
+        dat = dat >> 4;
+    }
+    out[8]=0;
+
+    puts(out);
+}
+
+int main(int argc, char **argv)
+{
+    __uint32_t config=0;
+    __uint32_t status=0;
+    __uint32_t bus=0;
+	int t;
+
+	t=counter;
+	for (;;)
+	{
+        config = read_raw((__uint32_t *)ZWC_CONFIG);
+        status = read_raw((__uint32_t *)ZWC_STATUS);
+        bus = read_raw((__uint32_t *)ZWC_0_BASE);
+
+        puts("c:");
+        print_hex(config);
+        puts("s:");
+        print_hex(status);
+        puts("b:");
+        print_hex(bus);
+        //printf("config = 0x%x, status=0x%s\n", config, status);
+
+        write_raw((__uint32_t *)ZWC_CONFIG, 0x87654321);
+        write_raw((__uint32_t *)ZWC_STATUS, 0x12345678);
+        write_raw((__uint32_t *)ZWC_0_BASE, 0x9abcdef0);
+
+        config = read_raw((__uint32_t *)ZWC_CONFIG);
+        status = read_raw((__uint32_t *)ZWC_STATUS);
+        bus = read_raw((__uint32_t *)ZWC_0_BASE);
+
+        puts("c:");
+        print_hex(config);
+        puts("s:");
+        print_hex(status);
+        puts("b:");
+        print_hex(bus);
+        //printf("config = 0x%x, status=0x%s\n", config, status);
+	}
+    
+}
