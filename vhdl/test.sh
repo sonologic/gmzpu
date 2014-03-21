c345a7577c23445b48f8fc01dc8f4d68cb4f8c11
Koen Martens <gmc@sonologic.nl>
2014-03-21 20:28:55 +0100

    sim: run unit tests in disposable directory

--- vhdl/test.sh
+++ vhdl/test.sh
@@ -8,10 +8,19 @@ NO='\e[0m'
 OK=0
 FAIL=0
 
-for d in `find . -type d -name test`; do
+DIR=`pwd`
+
+for d in `find $PWD -type d -name test`; do
     cd ${d}
     for f in *.do; do
         echo -en "[    ] ${d}/${f}"
+        OLDDIR=`pwd`
+        tb=`echo $f | sed 's/\.do/_tb.vhdl/g'`
+        rm -Rf ../work
+        mkdir -p ../work
+        cp $f ../work/
+        cp $tb ../work/
+        cd ../work        
         vsim -c -assertfile $f.assert -do $f > /dev/null
         if [ `wc -l $f.assert | cut -d\  -f 1` -eq 0 ]; then
             echo -e "\r[${GREEN}OK  ${NO}"
@@ -20,10 +29,14 @@ for d in `find . -type d -name test`; do
             echo -e "\r[${RED}FAIL${NO}"
             FAIL=$(( ${OK} + 1 ))
         fi
-        rm -f $f.assert
+        cd ${d}
+        #cd ${OLDDIR}
+        rm -Rf ../work
     done
-    cd - > /dev/null
+    #cd - > /dev/null
 done
+
+cd ${DIR}
 
 if [ ${FAIL} -eq 0 ]; then
     echo -en "${GREEN}"
